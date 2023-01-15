f7e2e36b8eb7b6c11c650c43a1120618465f9ce1
rhkrhyunmin <rhkrhyunmin@gmail.com>
2023-01-16 00:00:22 +0900

    1.16

--- Assets/03.Script/PlayerManager.cs
+++ Assets/03.Script/PlayerManager.cs
@@ -6,16 +6,20 @@ public class PlayerManager : MonoBehaviour
 {
     public Vector2 vector3;
     public Rigidbody2D rigidbody2D;
+    public bool isJumping;
     public float speed = 10;
+    public float jumpSpeed = 10;
 
     public void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
+        
     }
 
     public void Update()
     {
         Move();
+        Jump();
     }
 
     public void Move()
@@ -23,7 +27,24 @@ public class PlayerManager : MonoBehaviour
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
 
-        vector3 = new Vector2(x, y);
+        vector3 = new Vector2(x,0);
         transform.position += (Vector3) (vector3 * speed * Time.deltaTime);
+    }
+
+    public void Jump()
+    {
+        if (Input.GetKey(KeyCode.Space)&& isJumping == false)
+        {
+            rigidbody2D.AddForce(Vector3.up * jumpSpeed, ForceMode2D.Impulse);
+            isJumping = true;
+        }
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            isJumping=false;
+        }
     }
 }
