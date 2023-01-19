3879bc05a0499538bdbea5760cf9f3728848636b
rhkrhyunmin <rhkrhyunmin@gmail.com>
2023-01-19 18:00:47 +0900

    1.19

--- Assets/03.Script/hyunmin/Enemy/EnemyManager.cs
+++ Assets/03.Script/hyunmin/Enemy/EnemyManager.cs
@@ -9,6 +9,9 @@ public class EnemyManager : MonoBehaviour
     [Header("위치")]
     private Transform target;
     private NavMeshAgent navMesh;
+    [Header("체력")]
+    public float CurrentHp;
+    public float MaxHp = 10;
 
     public void Awake()
     {
@@ -19,5 +22,23 @@ public class EnemyManager : MonoBehaviour
     public void Update()
     {
         navMesh.SetDestination(target.position);
+        PlayerFind();
+    }
+
+    public void PlayerFind()
+    {
+        float dis = Vector3.Distance(transform.position ,target.position);
+        if (dis <= 5)
+        {
+            Move();
+        }
+        else return;
+    }
+
+    public void Move()
+    {
+        float dir = target.position.x - transform.position.x;
+        dir = (dir < 0) ? -1 : 1;
+        transform.position = new Vector2(dir, 0) * speed * Time.deltaTime;
     }
 }
--- Assets/03.Script/hyunmin/Player/Bullet.cs
+++ Assets/03.Script/hyunmin/Player/Bullet.cs
@@ -4,12 +4,22 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    EnemyManager Enemy;
+    [Header("거리")]
     public float speed = 15;
     public float time = 0;
+    [Header("데미지")]
+    public float damage = 3;
 
-    void Update()
+    public void Start()
+    {
+        Enemy = GetComponent<EnemyManager>();
+    }
+
+    public void Update()
     {
         DistoryBullet();
+        Damage();
     }
 
     public void DistoryBullet()
@@ -21,4 +31,25 @@ public class Bullet : MonoBehaviour
         }
         this.transform.Translate(Vector3.left * this.speed * Time.deltaTime);
     }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Enemy")
+        {
+            Enemy.CurrentHp -= damage;
+            Destroy(collision.gameObject);
+            if(Enemy.CurrentHp < 0)
+            {
+                Destroy(collision.gameObject);
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    public void Damage()
+    {
+       
+    }
+
+    
 }
